815d571da6adbefe1b820f22b6dd4854e09c1e71
Chris Borgia <me@cborgia.me>
2017-03-11 20:17:56 -0500

    backlog ahead of update
    these file edits occurred a while back, just committing now ahead of new efforts

--- src/scripts/app.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-    $("#typed").typed({
-        strings: ["Quickly get going ^1000 with a web project.", "I hope you like!"]
-    });
-});
---
+++ src/scripts/main.js
@@ -0,0 +1,5 @@
+$(function(){
+    $("#typed").typed({
+        strings: ["Quickly get going ^1000 with a web project.", "Process Sass", "Sync my Browsers", "I hope you like..."]
+    });
+});
