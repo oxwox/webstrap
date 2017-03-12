20abfaa5a6deef142f2502971b65cb05a490931d
Chris Borgia <me@cborgia.me>
2017-03-12 13:14:01 -0400

    wip

--- buildscripts/srcServer.js
+++ buildscripts/srcServer.js
@@ -2,17 +2,17 @@ var express = require('express');
 var path = require('path');
 var open = require('open');
 
-var port = 3300;
+var port = 3333;
 var app = express();
 
-app.get('/', function(req,res) {
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 app.use(express.static('dist'));
 
 app.listen(port, function(err) {
-    if(err) {
+    if (err) {
         console.log(err);
     } else {
         open('http://localhost:' + port);
